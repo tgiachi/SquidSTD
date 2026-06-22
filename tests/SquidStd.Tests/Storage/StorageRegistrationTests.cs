e494c7ff8dc6655927c904375f0b6951556a849e
Tom <squid@stormwind.it>
2026-06-22 20:52:43 +0200

    test(storage): add AddFileStorage registration test

---
+++ tests/SquidStd.Tests/Storage/StorageRegistrationTests.cs
@@ -0,0 +1,28 @@
+using DryIoc;
+using SquidStd.Storage.Abstractions.Data.Config;
+using SquidStd.Storage.Abstractions.Interfaces;
+using SquidStd.Storage.Extensions;
+
+namespace SquidStd.Tests.Storage;
+
+public class StorageRegistrationTests
+{
+    [Fact]
+    public async Task AddFileStorage_ResolvesAndRoundTrips()
+    {
+        var root = Path.Combine(Path.GetTempPath(), "squidstd-storage-" + Guid.NewGuid().ToString("N"));
+        var container = new Container();
+
+        container.AddFileStorage(new StorageConfig { RootDirectory = root });
+
+        var storage = container.Resolve<IStorageService>();
+        Assert.NotNull(container.Resolve<IObjectStorageService>());
+
+        await storage.SaveAsync("k", new byte[] { 1, 2, 3 });
+        var loaded = await storage.LoadAsync("k");
+
+        Assert.Equal(new byte[] { 1, 2, 3 }, loaded);
+
+        Directory.Delete(root, recursive: true);
+    }
+}
